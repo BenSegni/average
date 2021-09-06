e7dc2a73ac22a8c488c9ba3c6347e46ba7cebbfe
Ben Segni <BSegni@unilink.com>
2021-09-06 16:37:58 +0100

    spread operator to allow mutliple inputs

--- average.js
+++ average.js
@@ -1,7 +1,11 @@
 var averageResult;
-function calculateAverage(numberArray) {
+function calculateAverage() {
+    var numberArray = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numberArray[_i] = arguments[_i];
+    }
     averageResult = numberArray.reduce(function (a, b) { return a + b; }) / numberArray.length;
     return averageResult;
 }
-var result = calculateAverage([1, 2, 3, 4, 5]);
+var result = calculateAverage(1, 2, 3, 4, 5, 6);
 console.log("average:", result);
--- average.ts
+++ average.ts
@@ -1,11 +1,11 @@
 let averageResult: number;
 
-function calculateAverage(numberArray: number[]): number {
+function calculateAverage(...numberArray: number[]): number {
   averageResult = numberArray.reduce((a, b) => a + b) / numberArray.length;
 
   return averageResult;
 }
 
-let result: number = calculateAverage([1, 2, 3, 4, 5]);
+let result: number = calculateAverage(1, 2, 3, 4, 5, 6);
 
 console.log("average:", result);
